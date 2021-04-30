1ab976f179309d61d1acb5a0f2f219236014f7bf
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2021-04-30 14:59:16 +0200

    created project files

---
+++ main.c
@@ -0,0 +1,4 @@
+//
+// Created by vinanddrinks on 30/04/2021.
+//
+
---
+++ rubixdream.h
@@ -0,0 +1,8 @@
+//
+// Created by vinanddrinks on 30/04/2021.
+//
+
+#ifndef RUBIXDREAM_RUBIXDREAM_H
+#define RUBIXDREAM_RUBIXDREAM_H
+
+#endif //RUBIXDREAM_RUBIXDREAM_H
