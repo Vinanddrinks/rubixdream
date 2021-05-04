c5909bf802ebe0c59698184150d1a84550d16d43
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2021-05-04 10:58:17 +0200

    retry conio installation

---
+++ conio.c
@@ -0,0 +1,5 @@
+//
+// Created by vinan on 04/05/2021.
+//
+
+#include "conio.h"
---
+++ conio.h
@@ -0,0 +1,8 @@
+//
+// Created by vinan on 04/05/2021.
+//
+
+#ifndef RUBIXDREAM_CONIO_H
+#define RUBIXDREAM_CONIO_H
+
+#endif //RUBIXDREAM_CONIO_H
