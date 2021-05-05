0ce95a0739b88db536523c7559c7a8671f9b58a4
Vinanddrinks <66210430+Vinanddrinks@users.noreply.github.com>
2021-05-05 13:55:57 +0200

    prototype of rubiks

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 // Created by vinanddrinks on 30/04/2021.
 //
 #include "rubixdream.h"
+#include "conio.h"
+#include "rubixdream.c"
+#include <stdio.h>
 
 int main (){
     return 0;
--- rubixdream.h
+++ rubixdream.h
@@ -4,4 +4,14 @@
 #include <stdio.h>
 #ifndef RUBIXDREAM_RUBIXDREAM_H
 #define RUBIXDREAM_RUBIXDREAM_H
+
+typedef enum{FRONT, BACK,UP,DOWN,RIGHT,LEFT }T_SIDE;
+typedef enum{R, B, G, W, Y, O, LG}T_COLOR;
+struct FACE{
+    T_SIDE SIDE;
+    T_COLOR BLOCK[3][3];
+};
+
+
+
 #endif //RUBIXDREAM_RUBIXDREAM_H
